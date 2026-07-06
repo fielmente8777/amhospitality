b6852362381816c218b8d58f9805ede24a7f96ce
Personal <sk7627577@gmail.com>
2026-07-06 21:41:39 +0530

    up

--- src/utils/constent.tsx
+++ src/utils/constent.tsx
@@ -1,16 +1,16 @@
 export const enCodeText = encodeURIComponent(`Hello Team,
 
-I would like to enquire about booking a stay at swan suites.
+I would like to enquire about booking a stay at AM Hospitality.
 
 Please share room availability and best offers.
 
 Thank you.`);
 
 export const contact = {
-  email: ["bookings@swansuites.com ", " swansuites@gmail.com"],
+  email: ["dwm0S@example.com"],
   mapUrl:
     "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3618.1658189756754!2d78.3666466!3d17.43535!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bcb9397d994815f%3A0x6f1af95f92b4b017!2sSwan%20Suites%20Gachibowli!5e1!3m2!1sen!2sin!4v1780822837324!5m2!1sen!2sin",
-  phone: ["+91 9642244000 ", "+91 9642244000"],
+  phone: ["+91 9XXXX XXXX",],
   address:
     "Sri Shyam Nagar, Telecom Nagar, Gachibowli, Hyderabad, Telangana 500032",
   addressLink: "https://maps.app.goo.gl/tXFB7KaW1XViipsy5",
@@ -37,10 +37,10 @@ export const contact = {
       addressLink: "https://maps.app.goo.gl/zxyGThZCvNRLRX8p7",
     },
   },
-  callCta: `tel:+919642244000`,
-  WhatsappCta: `https://wa.me/+919642244000?text=${enCodeText}`,
-  formDomain: "swansuites",
-  // formDomain: "testmulti", //for testing
+  callCta: `tel:+919xxxxxxxx`,
+  WhatsappCta: `https://wa.me/+919xxxxxxxx?text=${enCodeText}`,
+  // formDomain: "",
+  formDomain: "testmulti", //for testing
   formHid: "",
   // formHid: "",
 };
